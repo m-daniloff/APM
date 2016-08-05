54eeb16c4b2b988d8006d94f61d8685767f55134
Michael Danloff <michael.daniloff@esqinc.com>
2016-08-05 08:39:26 -0700

    before Tying routes to actions....

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from 'angular2/core';
 import { HTTP_PROVIDERS } from 'angular2/http';
 import 'rxjs/Rx'; // Load all features
+import { ROUTER_PROVIDERS, RouteConfig } from 'angular2/router';
 
 import {ProductListComponent} from './products/product-list.component';
 import { ProductService } from './products/product.service';
+import { WelcomeComponent } from './home/welcome.component';
 @Component({
     selector: 'pm-app',
     template: `
@@ -12,8 +14,13 @@ import { ProductService } from './products/product.service';
     </div>
     `,
     directives: [ProductListComponent],
-    providers: [ProductService, HTTP_PROVIDERS]
+    providers: [ProductService, HTTP_PROVIDERS, ROUTER_PROVIDERS]
 })
+
+@RouteConfig([
+    { path: '/welcome', name: 'Welcome', component: WelcomeComponent, useAsDefault: true },
+    { path: '/products', name: 'Products', component: ProductListComponent }
+])
 
 export class AppComponent {
     pageTitle: string = 'Acme Product Management';
